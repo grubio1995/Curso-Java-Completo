bfaa6afecb270639c4072853b2bb884eaae8c5fb
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-03-02 23:32:18 -0300

    Corrigido a descrição do exercício 5

--- Estrutura sequencial/SomaProdutos/src/SomaProdutos.java
+++ Estrutura sequencial/SomaProdutos/src/SomaProdutos.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * 
  * @author Gabriel Rubio
  *
- * Exercício 1 - Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor 
+ * Exercício 5 - Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor 
  * unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada 
  * peça 2. Calcule e mostre o valor a ser pago.
  *
