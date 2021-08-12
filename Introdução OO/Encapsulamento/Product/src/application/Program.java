f9049f851cbe9a6d5a78b057db68bebe6221d9a4
grubio1995 <gabriel_pinhatarubio@hotmail.com>
2021-08-12 17:28:59 -0300

    Exercício fixação - Encapsulamento

---
+++ Introdução OO/Encapsulamento/Product/src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+/**
+ ** @author Gabriel Rubio
+ *
+ *         Fazer um programa para ler os dados de um produto em estoque (nome,
+ *         preço e quantidade no estoque). Em seguida: • Mostrar os dados do
+ *         produto (nome, preço, quantidade no estoque, valor total no estoque)
+ *         • Realizar uma entrada no estoque e mostrar novamente os dados do
+ *         produto • Realizar uma saída no estoque e mostrar novamente os dados
+ *         do produto
+ * 
+ */
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		Product product = new Product(name, price);
+
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
+		
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+	    int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		sc.close();
+	}
+}
