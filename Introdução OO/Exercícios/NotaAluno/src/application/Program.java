56fcca9d9bf1f18cce21c32f45e91931277314d6
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-08-30 22:04:21 -0300

    Organização das pastas - Introdução OO

--- Introdução OO/Exercícios/NotaAluno/src/application/Program.java
+++ Introdução OO/Exercícios/NotaAluno/src/application/Program.java
@@ -12,7 +12,7 @@ import entities.Student;
  *         notas que ele obteve nos três trimestres do ano (primeiro trimestre
  *         vale 30, e o segundo e terceiro valem 35 cada). Ao final, mostrar
  *         qual a nota final do aluno no ano. Dizer também se o aluno está
- *         aprovadi (PASS) ou não (FAILED) e, em caso negativo, quantos pontos
+ *         aprovado (PASS) ou não (FAILED) e, em caso negativo, quantos pontos
  *         faltam para o aluno obter o mínimo para ser aprovado que é (60% da
  *         nota). Você deve criar uma classe Student para resolver este problema.
  * 
